878d270a232ae6badede7ae8da8fcd4cc805eb55
zhss1983 <zhss1983@gmail.com>
2021-07-09 17:40:58 +0500

    Подправил комментарии.

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,12 +1,12 @@
 import debug_toolbar
 from django.conf import settings
-from django.conf.urls import handler404, handler500 # noqa
+from django.conf.urls import handler404, handler500  # noqa
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-handler404 = 'posts.views.page_not_found' # noqa
-handler500 = 'posts.views.server_error' # noqa
+handler404 = 'posts.views.page_not_found'  # noqa
+handler500 = 'posts.views.server_error'  # noqa
 
 urlpatterns = [
     path('about/', include('about.urls', namespace='about')),
